d882f9533a844da777870ec42a056c533f62f140
Lucas Anastacio <lucas_anastacio@hotmail.com>
2022-02-08 12:35:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller Update: ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
+	
 
 }
